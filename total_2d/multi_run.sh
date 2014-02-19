11534c4adbf9e667b199af2f7bce2ad3682545b0
Vasily Golubev <w.golubev@mail.ru>
2014-02-18 20:51:48 -0500

    Fixed config.

--- total_2d/multi_run.sh
+++ total_2d/multi_run.sh
@@ -45,17 +45,17 @@
 # dt, steps, size, spacing, name, save, file
 NAME=""
 echo "Generated automatically." > summary-multi.txt
-NAME="AdvRectSchemaCIR"
+NAME="AdvRectSchemaCIP23Ext"
 echo $NAME >> summary-multi.txt
-./prepare.py 0.004 501,5001 100,100 0.02,0.02 $NAME 500 000500
+./prepare.py 0.004 501 100,100 0.02,0.02 $NAME 500 000500
 ./single_run.sh
-./prepare.py 0.002 1001,1001 200,200 0.01,0.01 $NAME 1000 001000
+./prepare.py 0.002 1001 200,200 0.01,0.01 $NAME 1000 001000
 ./single_run.sh
-./prepare.py 0.001 2001,2001 400,400 0.005,0.005 $NAME 2000 002000
+./prepare.py 0.001 2001 400,400 0.005,0.005 $NAME 2000 002000
 ./single_run.sh
-./prepare.py 0.0005 4001,4001 800,800 0.0025,0.0025 $NAME 4000 004000
+./prepare.py 0.0005 4001 800,800 0.0025,0.0025 $NAME 4000 004000
 ./single_run.sh
-./prepare.py 0.00025 8001,8001 1600,1600 0.00125,0.00125 $NAME 8000 008000
+./prepare.py 0.00025 8001 1600,1600 0.00125,0.00125 $NAME 8000 008000
 ./single_run.sh
 
 cat summary-multi.txt
